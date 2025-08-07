9f5b48f19da3ebf0f88162a25c65ab80990b7186
null <null>
2025-08-07 16:25:09 +0100

    Quick update to the landing page video with Olu

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -331,12 +331,15 @@ const handleLoginClick = () => {
           {/* Outer div for responsive aspect ratio (16:9 - 56.25%) */}
           <div style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
             <iframe
-              src="https://player.vimeo.com/video/1096029698?badge=0&autopause=0&player_id=0&app_id=58479"
+              //src="https://player.vimeo.com/video/1096029698?badge=0&autopause=0&player_id=0&app_id=58479"
+              // Above video works fine. 
+
+              src="https://player.vimeo.com/video/1108122437?badge=0&autopause=0&player_id=0&app_id=58479"
               frameBorder="0"
               allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-share" // Ensure `encrypted-media` is included for broader compatibility
               allowFullScreen
               style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-              title="Latest_SoSavvy_Video_Design"
+              //title="Latest_SoSavvy_Video_Design"
             ></iframe>
           </div>
         </div>
