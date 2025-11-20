d12f201094601de147d60abd0fdd288f0885b535
OluStayhired <olu@prelo.io>
2025-11-20 08:30:05 +0000

    made minor changes to main message coaches

--- src/pages/CoachesPage.tsx
+++ src/pages/CoachesPage.tsx
@@ -309,7 +309,7 @@ const handleLoginClick = () => {
       <main className="max-w-7xl mx-auto px-6 pt-10 pb-32">
         <div className="text-center px-4 sm:px-6 md:px-8 lg:px-12 py-12 sm:py-16 md:py-20 lg:py-24 rounded-lg">
           
-        <span className="text-sm sm:text-lg p-3 font-semibold bg-blue-100 rounded-full text-blue-500 border-8 border-blue-50">Simplify Content Creation for your Coaching Business</span>
+        <span className="text-xs sm:text-lg p-3 font-semibold bg-blue-100 rounded-full text-blue-500 border-8 border-blue-50">Simplify Content Creation for your Business</span>
           
            {/*start alternative header */}
     
