2293c15e56554675693433cb478d20c15035b8a4
OluStayhired <olu@prelo.io>
2025-11-26 14:53:44 +0000

    fixed App.tsx for Poetiq Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { BlogPostPage } from './components/BlogPostPage';
 import AccountantPage from './pages/AccountantPage';
 import CoachesPage from './pages/CoachesPage';
 import RecruitersPage from './pages/RecruitersPage';
-import { PoetiqPage } from './pages/PoetiqLandingPage';
+import PoetiqPage from './pages/PoetiqLandingPage';
 
 function PrivateRoute({ children }: { children: React.ReactNode }) {
   //const { isAuthenticated } = useAuth();
@@ -49,7 +49,7 @@ function AppRoutes() {
       <Route path="/blog" element={<BlogListPage />} />
       <Route path="blog/:slug" element={<BlogPostPage />} /> 
       <Route path="/linkedin" element={<AccountantPage />} />
-      <Route path="/poetiq" element={<AccountantPage />} />
+      <Route path="/poetiq" element={<PoetiqPage />} />
       <Route path="linkedin/linkedin-for-accountants" element={<AccountantPage />} />
       <Route path="linkedin/linkedin-for-coaches" element={<CoachesPage />} />
       <Route path="linkedin/linkedin-for-recruiters" element={<RecruitersPage />} />
